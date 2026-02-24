c0ebf25b779c1ab7f779c27978b2f0d2f42581b0
Qualityetime <ethanwansel@vt.edu>
2026-02-24 15:26:08 -0500

    Update sketch.js

--- Project 1/sketch.js
+++ Project 1/sketch.js
@@ -4,10 +4,65 @@ function setup(){
 }
 
 function draw(){
-background(0);
+background(200);
     orbitControl();
      noStroke();
     lights();
+    wristPic();
 }
 
-function 
+function wristPic() {
+    translate(-10, 140);
+  box(100, 20, 200);
+    push();
+    rotate(45);
+    translate(-90, 35);
+    box(100, 20, 200);
+    pop();
+    push();
+     rotate(-45);
+    translate(90, 35);
+     box(100, 20, 200);
+    pop();
+    push();
+    rotate(90);
+    translate(-130, -125);
+     box(100, 20, 200);
+    pop();
+    push();
+    rotate(90);
+    translate(-130, 125);
+     box(100, 20, 200);
+    pop();
+    push();
+    rotate(-45);
+    translate(90, -215);
+     box(100, 20, 200);
+    pop();
+    push();
+    rotate(45);
+    translate(-90, -215);
+     box(100, 20, 200);
+    pop();
+    push();
+    translate(0,-250);
+      box(100, 20, 200);
+    pop();
+    push();
+    rotateX(90);
+    translate(0, 90 - 10, 260);
+    cylinder(20, 40);
+    pop();
+    push();
+    fill('black');
+    translate(0,-260, -15);
+      box(80, 10, 140);
+    pop();
+    push();
+    fill('blue');
+    translate(0,-260.1, -15);
+      box(70, 10, 120);
+    pop();
+    
+    
+}
