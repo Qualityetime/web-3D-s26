f780d09d146e0127aa4954e09b172482112fabc8
Qualityetime <ethanwansel@vt.edu>
2026-02-24 13:09:13 -0500

    improve

---
+++ hw4/sketch.js
@@ -0,0 +1,14 @@
+function setup(){
+    let canvas = createCanvas(400, 400, WEBGL);
+    angleMode(DEGREES);
+}
+
+function draw(){
+background(0);
+    orbitControl();
+     noStroke();
+    lights();
+}
+
+function 
+
