8939a51852a640b6785c8ffc6a9dcd3bb8e77aa7
Qualityetime <ethanwansel@vt.edu>
2026-02-24 13:12:25 -0500

    Update sketch.js

---
+++ Project 1/sketch.js
@@ -0,0 +1,13 @@
+function setup(){
+    let canvas = createCanvas(400, 400, WEBGL);
+    angleMode(DEGREES);
+}
+
+function draw(){
+background(0);
+    orbitControl();
+     noStroke();
+    lights();
+}
+
+function 
