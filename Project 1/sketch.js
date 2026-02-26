5421aa947ee754205d7ed0141fd0590b1b94550b
Qualityetime <ethanwansel@vt.edu>
2026-02-25 19:59:43 -0500

    color and material edit

--- Project 1/sketch.js
+++ Project 1/sketch.js
@@ -1,93 +1,106 @@
-function setup(){
+let bandColor;
+function setup() {
     let canvas = createCanvas(400, 400, WEBGL);
     angleMode(DEGREES);
+    bandColor = color(170, 170, 185);
     let button = createButton("Customize Color");
     button.parent("button-holder");
     button.mousePressed(customizeColor);
 }
 
-function draw(){
-background(200);
+function draw() {
+    background(200);
     orbitControl();
-     noStroke();
-    lights();
+    noStroke();
+    ambientLight(50);
+    directionalLight(255, 255, 255, -0.5, -0.7, -1);
+    pointLight(255, 255, 255, 200, -200, 300);
     wristPic();
 }
 
 function wristPic() {
     translate(-10, 140);
-  box(100, 20, 200);
+    push();
+    metalMat(bandColor, 90);
+    box(100, 20, 200);
     push();
     rotate(45);
     translate(-90, 35);
     box(100, 20, 200);
     pop();
     push();
-     rotate(-45);
+    rotate(-45);
     translate(90, 35);
-     box(100, 20, 200);
+    box(100, 20, 200);
     pop();
     push();
     rotate(90);
     translate(-130, -125);
-     box(100, 20, 200);
+    box(100, 20, 200);
     pop();
     push();
     rotate(90);
     translate(-130, 125);
-     box(100, 20, 200);
+    box(100, 20, 200);
     pop();
     push();
     rotate(-45);
     translate(90, -215);
-     box(100, 20, 200);
+    box(100, 20, 200);
     pop();
     push();
     rotate(45);
     translate(-90, -215);
-     box(100, 20, 200);
+    box(100, 20, 200);
     pop();
     push();
-    translate(0,-250);
-      box(100, 20, 200);
+    translate(0, -250);
+    box(100, 20, 200);
     pop();
     push();
     rotateX(90);
     translate(0, 90 - 10, 260);
     cylinder(20, 40);
     pop();
-    push();
-    fill('black');
-    translate(0,-260, -15);
-      box(80, 10, 140);
     pop();
     push();
-    fill('blue');
-    translate(0,-260.1, -15);
-      box(70, 10, 120);
+    fill("black");
+    translate(0, -260, -15);
+    box(80, 10, 140);
+    pop();
+    push();
+    fill("blue");
+    translate(0, -260.1, -15);
+    box(70, 10, 120);
     pop();
     push();
     fill(100, 100, 100, 100);
-    translate(0,-260, 95);
+    translate(0, -260, 95);
     sphere(15);
     pop();
     push();
-    translate(0,-260, 99);
-    fill('black');
+    translate(0, -260, 99);
+    fill("black");
     sphere(13);
     pop();
-     push();
-    translate(0,-260, 100.5);
+    push();
+    translate(0, -260, 100.5);
     fill(100, 100, 100, 100);
     sphere(12);
     pop();
-     push();
-    translate(0,-260, 105);
-    fill('gray');
+    push();
+    translate(0, -260, 105);
+    fill("gray");
     sphere(8);
     pop();
 }
 
+function metalMat(bandColor, shine = 80) {
+    ambientMaterial(bandColor);
+    specularMaterial(230);
+    shininess(shine);
+}
+
 function customizeColor() {
-fill(color(random(40, 255), random(40, 255), random(40, 255)));   
+    bandColor = color(random(40, 255), random(40, 255), random(40, 255));
 }
