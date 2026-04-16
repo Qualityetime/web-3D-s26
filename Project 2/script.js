b870ba7e3983f9505f1ee79689baf39b03c5724d
Qualityetime <ethanwansel@vt.edu>
2026-04-16 14:59:57 -0400

    images

--- Project 2/script.js
+++ Project 2/script.js
@@ -238,19 +238,75 @@ function init() {
     display1.position.set(0, 5, -244.5);
     scene.add(display1);
 
+    const godzillaTexture = new THREE.TextureLoader().load("./assets/godzilla.png");
+    const godzillaMat = new THREE.MeshBasicMaterial({
+        map: godzillaTexture,
+        transparent: true,
+        side: THREE.DoubleSide
+    });
+
+    const godzillaPoster = new THREE.Mesh(displayGeo1, godzillaMat);
+    godzillaPoster.position.set(0, 5, -244.0);
+    scene.add(godzillaPoster);
+
     // back wall side displays
     const displayGeo2 = new THREE.PlaneGeometry(45, 60);
+
+    const pulpTexture = new THREE.TextureLoader().load("./assets/pulpfiction.png");
+    const pulpMat = new THREE.MeshBasicMaterial({
+        map: pulpTexture,
+        transparent: true,
+        side: THREE.DoubleSide
+    });
+
+    const pulpPoster = new THREE.Mesh(displayGeo2, pulpMat);
+    pulpPoster.position.set(-100, 0, 244.0);
+    pulpPoster.rotation.y = Math.PI;
+    scene.add(pulpPoster);
 
     const display2 = new THREE.Mesh(displayGeo2, displayMat);
     display2.position.set(-95, 10, -244.5);
     scene.add(display2);
 
+    const oldPosterTexture = new THREE.TextureLoader().load("./assets/old poster.jpg");
+    const oldPosterMat = new THREE.MeshBasicMaterial({
+        map: oldPosterTexture,
+        transparent: true,
+        side: THREE.DoubleSide
+    });
+
+    const oldPoster = new THREE.Mesh(displayGeo2, oldPosterMat);
+    oldPoster.position.set(-95, 10, -244.0);
+    scene.add(oldPoster);
+
     const display3 = new THREE.Mesh(displayGeo2, displayMat);
     display3.position.set(95, 10, -244.5);
     scene.add(display3);
 
+    const conanTexture = new THREE.TextureLoader().load("./assets/conan.png");
+    const conanMat = new THREE.MeshBasicMaterial({
+        map: conanTexture,
+        transparent: true,
+        side: THREE.DoubleSide
+    });
+    const conanPoster = new THREE.Mesh(displayGeo2, conanMat);
+    conanPoster.position.set(95, 10, -244.0);
+    scene.add(conanPoster);
+
     // left wall displays
     const sideDisplayGeo = new THREE.PlaneGeometry(50, 70);
+
+    const hemanTexture = new THREE.TextureLoader().load("./assets/heman.png");
+    const hemanMat = new THREE.MeshBasicMaterial({
+        map: hemanTexture,
+        transparent: true,
+        side: THREE.DoubleSide
+    });
+
+    const hemanPoster = new THREE.Mesh(sideDisplayGeo, hemanMat);
+    hemanPoster.position.set(-144.0, 5, -10);
+    hemanPoster.rotation.y = Math.PI / 2;
+    scene.add(hemanPoster);
 
     const display4 = new THREE.Mesh(sideDisplayGeo, displayMat);
     display4.position.set(-144.5, 5, -10);
@@ -262,16 +318,49 @@ function init() {
     display5.rotation.y = Math.PI / 2;
     scene.add(display5);
 
+    const pressureTexture = new THREE.TextureLoader().load("./assets/pressure.png");
+    const pressureMat = new THREE.MeshBasicMaterial({
+        map: pressureTexture,
+        transparent: true,
+        side: THREE.DoubleSide
+    });
+    const pressurePoster = new THREE.Mesh(sideDisplayGeo, pressureMat);
+    pressurePoster.position.set(-144.0, 5, 110);
+    pressurePoster.rotation.y = Math.PI / 2;
+    scene.add(pressurePoster);
+
     // right wall displays
     const display6 = new THREE.Mesh(sideDisplayGeo, displayMat);
     display6.position.set(144.5, 5, -110);
     display6.rotation.y = -Math.PI / 2;
     scene.add(display6);
 
+    const monstermaskTexture = new THREE.TextureLoader().load("./assets/monstermask.png");
+    const monstermaskMat = new THREE.MeshBasicMaterial({
+        map: monstermaskTexture,
+        transparent: true,
+        side: THREE.DoubleSide
+    });
+    const monstermaskPoster = new THREE.Mesh(sideDisplayGeo, monstermaskMat);
+    monstermaskPoster.position.set(144.0, 5, -110);
+    monstermaskPoster.rotation.y = -Math.PI / 2;
+    scene.add(monstermaskPoster);
+
     const display7 = new THREE.Mesh(sideDisplayGeo, displayMat);
     display7.position.set(144.5, 5, 10);
     display7.rotation.y = -Math.PI / 2;
     scene.add(display7);
+
+    const img1Texture = new THREE.TextureLoader().load("./assets/img1.png");
+    const img1Mat = new THREE.MeshBasicMaterial({
+        map: img1Texture,
+        transparent: true,
+        side: THREE.DoubleSide
+    });
+    const img1Poster = new THREE.Mesh(sideDisplayGeo, img1Mat);
+    img1Poster.position.set(144.0, 5, 10);
+    img1Poster.rotation.y = -Math.PI / 2;
+    scene.add(img1Poster);
 
     const display8 = new THREE.Mesh(displayGeo2, displayMat);
     display8.position.set(-100, 0, 244.5);
@@ -282,6 +371,17 @@ function init() {
     display9.position.set(100, 0, 244.5);
     display9.rotation.y = Math.PI;
     scene.add(display9);
+
+    const ticketStubTexture = new THREE.TextureLoader().load("./assets/ticketStub.jpg");
+    const ticketStubMat = new THREE.MeshBasicMaterial({
+        map: ticketStubTexture,
+        transparent: true,
+        side: THREE.DoubleSide
+    });
+    const ticketStubPoster = new THREE.Mesh(displayGeo2, ticketStubMat);
+    ticketStubPoster.position.set(100, 0, 244.0);
+    ticketStubPoster.rotation.y = Math.PI;
+    scene.add(ticketStubPoster);
 
     const display10 = new THREE.Mesh(sideDisplayGeo, displayMat);
     display10.position.set(144.5, 5, 90);
